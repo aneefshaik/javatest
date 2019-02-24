a41692b9fc93994479783a02e6f1a180c362cc5e
Cloud User <ec2-user@ip-172-31-20-15.us-east-2.compute.internal>
2019-02-24 09:01:29 +0000

    added

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public  class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
 
-        System.out.println("hello jenkin am trying webhook");
+        System.out.println("hello in jenkin am trying webhook in linux");
 
     }
 
