f55a671cfe78e4941897c18f43d18629530fcf7b
Cloud User <ec2-user@ip-172-31-20-15.us-east-2.compute.internal>
2019-02-24 06:09:37 +0000

    added to cr

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public  class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
 
-        System.out.println("Hello, World  bglr,karnataka,hyderabad");
+        System.out.println("hello jenkins");
 
     }
 
